ee95c3d89bd031ba81c874cbfefcb33a3ad90cfe
Jimmy Nguyen <jimvonguyen.com>
2017-10-07 15:18:02 -0700

    working draft2

--- controllers/trip-api-routes.js
+++ controllers/trip-api-routes.js
@@ -7,7 +7,15 @@ module.exports = function(app){
         var new_trip = req.body;
         console.log(new_trip);
         db.Trip.create(new_trip).then(data =>{
-            res.json(data);
+            var trip_id = data.id;
+            db.Trip.findOne({
+                where: {id:trip_id}
+            }).then(dbTrip => {
+                var returnData = {
+                    tripInfo: dbTrip
+                };
+                res.render("tripview",returnData);
+            })
         })
     })
 
--- public/js/custom.js
+++ public/js/custom.js
@@ -33,99 +33,99 @@
         
     });
 
-    //Login fields
-    var btnLogin = $('#btnLogin');
-    var btnSignUp = $('#btnSignUp');
-    var btnLogOut = $('#btnLogOut');
+  //   //Login fields
+  //   var btnLogin = $('#btnLogin');
+  //   var btnSignUp = $('#btnSignUp');
+  //   var btnLogOut = $('#btnLogOut');
   
-    btnLogin.on('click', function(){
-      event.preventDefault();
+  //   btnLogin.on('click', function(){
+  //     event.preventDefault();
   
-      var txtEmail = $('#txtEmail').val().trim();
-      var txtPassword = $('#txtPassword').val().trim();
+  //     var txtEmail = $('#txtEmail').val().trim();
+  //     var txtPassword = $('#txtPassword').val().trim();
   
-      var email = txtEmail;
-      var password = txtPassword;
+  //     var email = txtEmail;
+  //     var password = txtPassword;
   
-      var auth = firebase.auth();
+  //     var auth = firebase.auth();
   
-      var promise = auth.signInWithEmailAndPassword(email, password);
+  //     var promise = auth.signInWithEmailAndPassword(email, password);
   
-      promise.catch(e => {
-        console.log(e.message);
-        $('#signUpAlert').html('<h3>Error Please sign-in again or sign-up with us.</h3>');
-      });
+  //     promise.catch(e => {
+  //       console.log(e.message);
+  //       $('#signUpAlert').html('<h3>Error Please sign-in again or sign-up with us.</h3>');
+  //     });
   
-      console.log('email is: '+ txtEmail);
-      console.log('password is: '+ txtPassword);
+  //     console.log('email is: '+ txtEmail);
+  //     console.log('password is: '+ txtPassword);
   
-      txtEmail = $('#txtEmail').val('');
-      txtPassword = $('#txtPassword').val('');
+  //     txtEmail = $('#txtEmail').val('');
+  //     txtPassword = $('#txtPassword').val('');
   
-    });
+  //   });
   
-    //add a click for sign up
-    btnSignUp.on('click', function(){
-      event.preventDefault();
+  //   //add a click for sign up
+  //   btnSignUp.on('click', function(){
+  //     event.preventDefault();
   
-      //need to check for real password
-      var txtEmail = $('#txtEmail').val().trim();
-      //password should be at least 6 char
-      var txtPassword = $('#txtPassword').val().trim();
+  //     //need to check for real password
+  //     var txtEmail = $('#txtEmail').val().trim();
+  //     //password should be at least 6 char
+  //     var txtPassword = $('#txtPassword').val().trim();
   
-      var email = txtEmail;
-      var password = txtPassword;
+  //     var email = txtEmail;
+  //     var password = txtPassword;
   
-      var auth = firebase.auth();
+  //     var auth = firebase.auth();
   
-      var promise = auth.createUserWithEmailAndPassword(email, password);
+  //     var promise = auth.createUserWithEmailAndPassword(email, password);
   
-      promise.catch(e => {
-        console.log(e.message);
-        if (password.length <= 6){
-          $('#signUpAlert').html('<h3>Password must be longer than 6 characters</h3>');
-        }
-        else if (e.message = 'The email address is badly formatted.'){
-          $('#signUpAlert').html('<h3>Please use a correct email.</h3>');
-        }
-      });
+  //     promise.catch(e => {
+  //       console.log(e.message);
+  //       if (password.length <= 6){
+  //         $('#signUpAlert').html('<h3>Password must be longer than 6 characters</h3>');
+  //       }
+  //       else if (e.message = 'The email address is badly formatted.'){
+  //         $('#signUpAlert').html('<h3>Please use a correct email.</h3>');
+  //       }
+  //     });
   
   
-      console.log('email is: '+ txtEmail);
-      console.log('password is: '+ txtPassword);
+  //     console.log('email is: '+ txtEmail);
+  //     console.log('password is: '+ txtPassword);
   
-      txtEmail = $('#txtEmail').val('');
-      txtPassword = $('#txtPassword').val('');
+  //     txtEmail = $('#txtEmail').val('');
+  //     txtPassword = $('#txtPassword').val('');
   
-    });
+  //   });
   
-    btnLogOut.on('click', e => {
-      firebase.auth().signOut();
+  //   btnLogOut.on('click', e => {
+  //     firebase.auth().signOut();
   
-    });
+  //   });
   
-    //Add a realtime auth listen to see if user is logged in the whole time
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-      if(firebaseUser) {
-        console.log(firebaseUser);
-        //when users are logged in
-        btnLogOut.removeClass('hide');
-        btnLogin.addClass('hide');
-        $('#LoginInstructions').html('<p>You are logged in</p>');
-        $('.loginForm').addClass('hide');
-        $('#signUpAlert').html('');
+  //   //Add a realtime auth listen to see if user is logged in the whole time
+  //   firebase.auth().onAuthStateChanged(firebaseUser => {
+  //     if(firebaseUser) {
+  //       console.log(firebaseUser);
+  //       //when users are logged in
+  //       btnLogOut.removeClass('hide');
+  //       btnLogin.addClass('hide');
+  //       $('#LoginInstructions').html('<p>You are logged in</p>');
+  //       $('.loginForm').addClass('hide');
+  //       $('#signUpAlert').html('');
   
-      }
-      else {
-        //no users logged in
-        console.log('not Logged In');
-        btnLogOut.addClass('hide');
-        $('#LoginInstructions').html('<p>Already have an account?</p>'); 
-        $('.loginForm').removeClass('hide');     
-        $('#signUpAlert').removeClass('hide');
-        $('#signUpAlert').html('<p>Login Area</p>');
+  //     }
+  //     else {
+  //       //no users logged in
+  //       console.log('not Logged In');
+  //       btnLogOut.addClass('hide');
+  //       $('#LoginInstructions').html('<p>Already have an account?</p>'); 
+  //       $('.loginForm').removeClass('hide');     
+  //       $('#signUpAlert').removeClass('hide');
+  //       $('#signUpAlert').html('<p>Login Area</p>');
   
-      }
-   });   
+  //     }
+  //  });   
     
 })(jQuery);
